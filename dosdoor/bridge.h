68d9ba162d0511e3f568bc68149abbd1012364e1
Jerry Reed <jerry.reed@email.phoenix.edu>
2026-04-29 12:14:38 -0400

    Delete dosdoor directory

--- dosdoor/bridge.h
+++
@@ -1,9 +0,0 @@
-/* bridge.h - DOS-side API for the file-based door/helper bridge. */
-#ifndef ANET_DOS_BRIDGE_H
-#define ANET_DOS_BRIDGE_H
-void bridge_set_node(int n);
-int  bridge_connect_local(void);
-void bridge_close(void);
-int  bridge_send_line(const char *line);
-int  bridge_poll_line(char *out, int outsz);
-#endif
